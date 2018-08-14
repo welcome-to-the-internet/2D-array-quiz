1bd5bfc4a0576f0254f09c182420c85a33c6ca36
Uriel Reyes <uriel1012@yahoo.com>
2018-08-13 21:09:40 -0500

    Update quiz.js
    Code has been update to print out the questions/answers that were correct along with those that were incorrect

--- js/quiz.js
+++ js/quiz.js
@@ -4,26 +4,35 @@ var quiz = [["Does a cat meow or bark?", "meow"], ["Does a dog meow or bark?", "
 function print(message) {
   document.write(message);
 }
+var questionsRight = [];
+var questionsWrong = [];
+
 //quiz function which loops through each of the questions, gathers input and compares it to the stored answer value
 function quizRun(){
 var score = 0;
 var wrong = 0;
 var question = "";	
+var listCorrect = "<ol>";
+var listWrong = "<ol>";
 for(var i =0; i < quiz.length; i++){
 		var question = prompt(quiz[i][0]);
 		var playerAnswer = question;
 		if(playerAnswer === quiz[i][1]){
 			score += 1;
+			questionsRight.push(quiz[i][0]);
+			listCorrect += "<li>" + quiz[i] + "</li>";
 		}
 		else{
-			wrong +=1;						
+			wrong +=1;
+			questionsWrong.push(quiz[0]);
+			listWrong += "<li>" + quiz[i] + "</li>";						
 		}
 	}
 
 	print("You got " + score + " question(s) right!");
-	print("\n");
-	print("You got " + wrong + " question(s) wrong!");
-
+	print(listCorrect);
+	print("You got " + wrong + " question(s) wrong! \n");
+	print(listWrong);
 }
 
 quizRun();	
