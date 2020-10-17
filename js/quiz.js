1757fa12d3ec234b0914fcf88199e9a7edd24077
Kyle O'Brien <obrienk@webbhost.net>
2020-10-17 08:40:37 -0500

    Update quiz.js
    might be going the wrong way haha, fun

--- js/quiz.js
+++ js/quiz.js
@@ -1,5 +1,5 @@
 //2D array with question + answer
-var quiz = [["Does a cat meow or bark?", "meow"], ["Does a dog meow or bark?", "bark"], ["Does a duck meow or bark?", "neither"]];
+var quiz = [["cat or dog", ""], ["dragon or fairy?", ""], ["XaaS?", "fill in the blank (paragraph)"]];
 //default function
 function print(message) {
   document.write(message);
